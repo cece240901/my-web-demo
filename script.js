63fd6f2de63e4726cc2c29ae8f4e39324db18fa3
cece240901 <khj240901@senedu.kr>
2026-04-24 12:04:50 +0900

    Refactor color change logic into setupButtonCycle
    Refactored button color change functionality into setupButtonCycle function.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const colors = ["red", "blue", "green", "yellow", "purple"];
-let index = 0;
-
-document.getElementById("changeBtn").addEventListener("click", () => {
-  document.body.style.backgroundColor = colors[index];
-  index = (index + 1) % colors.length; // 끝까지 가면 다시 처음으로
-});
-
 function changeMessage() {
   document.getElementById("message").innerText = "버튼을 눌러 메시지가 바뀌었습니다!";
 }
+
+
+function setupButtonCycle() {
+  const colors = ["red", "blue", "green", "yellow", "purple"];
+  let index = 0;
+
+  const button = document.getElementById("changeBtn");
+
+  button.addEventListener("click", function() {
+    document.body.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length; // 배열 끝까지 가면 다시 처음으로
+  });
+}
+
+// 페이지가 로드되면 함수 실행
+window.onload = setupButtonCycle;

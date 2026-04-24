a8ab5625e19d78e40b6f6619e272d2dc8246384e
cece240901 <khj240901@senedu.kr>
2026-04-24 12:01:40 +0900

    Implement color change on button click
    Add functionality to change background color on button click.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const colors = ["red", "blue", "green", "yellow", "purple"];
+let index = 0;
+
+document.getElementById("changeBtn").addEventListener("click", () => {
+  document.body.style.backgroundColor = colors[index];
+  index = (index + 1) % colors.length; // 끝까지 가면 다시 처음으로
+});
+
 function changeMessage() {
   document.getElementById("message").innerText = "버튼을 눌러 메시지가 바뀌었습니다!";
 }
